ca619fb218d19bb874502496b0a23905a2dd877a
bijenSmartCode <bijen.smartcode@gmail.com>
2019-11-11 15:23:39 +0545

    reinit-LFT

--- js/main.js
+++
@@ -1,9 +0,0 @@
-var hang_word = new HangWord();
-
-if (hang_word.word === null) {
-    hang_word.getWord(function (word) {
-
-        var start_game = new StartGuess(word.word, word.hints,word.guess_count);
-        start_game.start();
-    });
-}

2d3fa1197ea7217bbc79ff6eb292846ba4975439
bijenSmartCode <bijen.smartcode@gmail.com>
2019-11-29 18:00:45 +0545

    speed changes

--- jsProjects/ast4/script.js
+++ jsProjects/ast4/script.js
@@ -4,14 +4,11 @@ if (app) {
     {posX: 10, posY: 10, height: 15, width: 45, color: 'blue', speed: 25},
     {posX: 10, posY: 10, height: 15, width: 45, color: 'blue', speed: 5},
     {posX: 10, posY: 10, height: 15, width: 45, color: 'blue', speed: 50},
-    {posX: 10, posY: 10, height: 50, width: 30,color: 'green', speed: 10},
-    {posX: 10, posY: 10, height: 30, width: 50,color: 'green', speed: 25},
-    {posX: 10, posY: 10, height: 15, width: 100,color: 'green', speed: 50},
     {posX: 10, posY: 10, height: 20, width: 80,color: 'black', speed: 20},
     {posX: 10, posY: 10, height: 40, width: 40,color: 'black', speed: 30},
     {posX: 10, posY: 10, height: 25, width: 64,color: 'black', speed: 40}
   ];
-  var boxCollision = new Game(app, 12);
+  var boxCollision = new Game(app, 15);
   boxCollision.height = 600;
   boxCollision.width = 600;
   boxCollision.init(boxObject);

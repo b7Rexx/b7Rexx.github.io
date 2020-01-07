9f85d07929cccd21ebc14f89046d06b4a01479b9
b7Rexx <bij.maharjan@gmail.com>
2020-01-07 18:34:20 +0545

    Updates

---
+++ precache-manifest.0a4953c2476de6b7b6305039c4b8ba14.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b9e87e5114ce2f95508ca17b2432c3b",
+    "url": "/ReactProjects/todo/index.html"
+  },
+  {
+    "revision": "4ad763d7a42aab7439ef",
+    "url": "/ReactProjects/todo/static/css/2.c078130e.chunk.css"
+  },
+  {
+    "revision": "cc33e0360607beb9d57f",
+    "url": "/ReactProjects/todo/static/css/main.451b7b60.chunk.css"
+  },
+  {
+    "revision": "4ad763d7a42aab7439ef",
+    "url": "/ReactProjects/todo/static/js/2.e2d76165.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ReactProjects/todo/static/js/2.e2d76165.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cc33e0360607beb9d57f",
+    "url": "/ReactProjects/todo/static/js/main.e34ae69f.chunk.js"
+  },
+  {
+    "revision": "1dd1a04982d5c72e0114",
+    "url": "/ReactProjects/todo/static/js/runtime-main.5baee557.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/ReactProjects/todo/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/ReactProjects/todo/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/ReactProjects/todo/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/ReactProjects/todo/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/ReactProjects/todo/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
